53b3881d0b4fc7a9509b00dfc94f1d7f96676e8f
SoneKosuke <SoneKosuke>
2015-01-25 15:37:02 +0800

    【こっち本当】撮影画像をAfterTakePhotVCに表示

---
+++ AfterTakePhotViewController.h
@@ -0,0 +1,15 @@
+//
+//  AfterTakePhotViewController.h
+//  Fuwarai_Test
+//
+//  Created by sonekousuke on 2015/01/24.
+//  Copyright (c) 2015年 KosukeSone. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface AfterTakePhotViewController : ViewController
+
+@property (weak, nonatomic) IBOutlet UIImageView *originImage;
+
+@end
